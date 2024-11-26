3bc2fd1df3a92f781e46c785a124dbabab90c48c
leonardopadilha <leonardopadilha28@gmail.com>
2024-11-26 08:35:07 -0300

    Insert new product without image

--- web/tests/e2e/products/products.spec.js
+++ web/tests/e2e/products/products.spec.js
@@ -1,11 +1,14 @@
 const { test } = require('../../support')
 const data = require('../../support/fixtures/users/users.json')
+const buy = require('../../support/fixtures/products/products.json')
 
 test('Insert new product through the link and without image', async ({ request, page }) => {
   await page.login.visit('/login')
   await page.login.formHaveText('Login')
 
   const user = data.admin
+  const product = buy.success
+
   await request.api.deleteUser(user)
   await request.api.registerUser(user)
   await page.login.form(user)
@@ -13,4 +16,8 @@ test('Insert new product through the link and without image', async ({ request, 
 
   await page.menu.clickOn('Cadastrar Produtos')
   await page.products.productsScreen()
+
+  await page.products.form(product)
+  await page.products.submit()
+  await page.products.showProductList()
 })
--- web/tests/support/actions/products/Products.js
+++ web/tests/support/actions/products/Products.js
@@ -9,6 +9,24 @@ export class Products {
     const text = await this.page.locator('h1')
     await expect(text).toHaveText('Cadastro de Produtos')
   }
+
+  async form(product) {
+    await this.page.locator('input[placeholder$="nome do produto"]').type(product.nome)
+    await this.page.getByTestId('preco').type(product.preco)
+    await this.page.getByTestId('descricao').type(product.descricao)
+    await this.page.getByTestId('quantity').type(product.quantidade)
+  }
+
+  async submit() {
+    await this.page.getByTestId('cadastarProdutos').click()
+  }
+
+  async showProductList() {
+    await expect(this.page).toHaveURL("https://front.serverest.dev/admin/cadastrarprodutos");
+
+/*     const rows = await this.page.locator('.table-striped')
+    await expect(rows).toContainText(product.nome) */
+  }
 }
 
 
